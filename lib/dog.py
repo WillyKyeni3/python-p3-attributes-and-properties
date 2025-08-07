3ded90aaaaec548ca13c77a8c2f1850431339ea6
willymusyoka <willykyeni@gmail.com>
2025-08-07 17:56:02 +0300

    Implement Dog and Person classes with validation for name and breed/job attributes

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,32 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="fido", breed=None):
+        self.name = name
+        self.breed = breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            self._name = None
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        elif value is not None:
+            print("Breed must be in list of approved breeds.")
+            self._breed = None
+        else:
+            self._breed = None
